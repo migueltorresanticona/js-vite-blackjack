5fca40f76fcc5dcb9667035bbc7695dc6ae41e99
Miguel Torres <miguel.torres.anticona@gmail.com>
2026-02-18 18:16:50 -0500

    set time out 200

--- src/blackjack/usecases/turno-computadora.js
+++ src/blackjack/usecases/turno-computadora.js
@@ -31,5 +31,5 @@ export const turnoComputadora = ( puntosMinimos,puntosHTML,divCartasComputadora,
         } else {
             alert('Computadora Gana')
         }
-    }, 100 );
+    }, 200 );
 }
